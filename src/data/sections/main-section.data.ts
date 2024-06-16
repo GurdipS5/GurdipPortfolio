5a880f75923533faca9799fce3b27c15299944b7
Gurdip <gurdip.sira@gmail.com>
2024-06-16 18:30:52 +0100

    s

--- src/data/sections/main-section.data.ts
+++ src/data/sections/main-section.data.ts
@@ -1,6 +1,6 @@
 import type { MainSection } from '@/types/sections/main-section.types';
 import type { ReadonlyDeep } from 'type-fest';
-import { facebook, github, linkedin, twitter } from '../helpers/links';
+import { medium, github, linkedin, twitter } from '../helpers/links';
 
 const mainSectionData = {
   config: {
@@ -10,30 +10,28 @@ const mainSectionData = {
     visible: true,
   },
   image: import('@/assets/my-image.jpeg'),
-  fullName: 'Mark Freeman',
-  role: 'Senior React Developer',
+  fullName: 'Gurdip Sira',
+  role: 'DevOps Lead',
   details: [
-    { label: 'Phone', value: '605 475 6961', url: 'tel:605 475 6961' },
-    { label: 'Email', value: 'mark.freeman.dev@gmail.com', url: 'mailto:mark.freeman.dev@gmail.com' },
-    { label: 'From', value: 'Warsaw, Poland' },
-    { label: 'Salary range', value: '18 000 - 25 000 PLN' },
+    { label: 'Email', value: 'gurdip.sira@gmail.com', url: 'mailto:mark.freeman.dev@gmail.com' },
+    { label: 'From', value: 'London, England' },
+    { label: 'Salary range', value: '100  000 - 110 000 GBP' },
   ],
   pdfDetails: [
-    { label: 'Phone', value: '605 475 6961' },
-    { label: 'Email', value: 'mark.freeman.dev@gmail.com' },
-    { label: 'LinkedIn', value: '/in/mark-freeman', url: 'https://linkedin.com' },
-    { label: 'GitHub', value: '/mark-freeman', url: 'https://github.com' },
-    { label: 'Website', value: 'mark-freeman-personal-website.com', url: '/', fullRow: true },
+    { label: 'Email', value: 'gurdip.sira@gmail.com' },
+    { label: 'LinkedIn', value: '/in/gurdipsira', url: 'https://linkedin.com' },
+    { label: 'GitHub', value: '/gurdips5', url: 'https://github.com' },
+    { label: 'Website', value: 'https://gurdipsira.dev', url: '/', fullRow: true },
   ],
   description:
-    'Lorem ipsum dolor sit amet, consectetur **adipiscing elit**. In sodales ac dui at *vestibulum*. In condimentum metus id dui tincidunt, in blandit mi [vehicula](/). Nulla lacinia, erat sit amet elementum vulputate, lectus mauris volutpat mi, vitae accumsan metus elit ut nunc. Vestibulum lacinia enim eget eros fermentum scelerisque. Proin augue leo, posuere ut imperdiet vitae, fermentum eu ipsum. Sed sed neque sagittis, posuere urna nec, commodo leo. Pellentesque posuere justo vitae massa volutpat maximus.',
-  tags: [{ name: 'Open for freelance' }, { name: 'Available for mentoring' }, { name: 'Working on side project' }],
+    'I am a dedicated and passionate DevOps Lead with a track record of driving seamless integration between development and operations. With over ten years of experience in the field, I specialize in implementing robust CI/CD pipelines, automating infrastructure, and fostering a collaborative DevOps culture within teams.',
+  tags: [{ name: 'Not open for work' }, { name: 'Available for mentoring' }, { name: 'Working on side projects and certs.' }],
   action: {
     label: 'Download CV',
     url: '/cv.pdf',
-    downloadedFileName: 'CV-Mark_Freeman.pdf',
+    downloadedFileName: 'CV-GurdipSira.pdf',
   },
-  links: [facebook({ url: '#' }), github({ url: '#' }), linkedin({ url: '#' }), twitter({ url: '#' })],
+  links: [ medium({url: ''}), github({ url: 'https://github.com/GurdipS5' }), linkedin({ url: '#' }), twitter({ url: '#' })],
 } as const satisfies ReadonlyDeep<MainSection>;
 
 export default mainSectionData;
